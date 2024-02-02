3bdd0dcdbbf8ec62a2a896b014fac4706e8931c8
JERRICK BEH ZHILUN 2204985B <2204985B@student.tp.edu.sg>
2024-02-03 04:07:43 +0800

    Added Blazor List, Create, Edit, View and delete function for staff, first and last name fields in register page.

--- GamePlatformManagement/Client/Program.cs
+++ GamePlatformManagement/Client/Program.cs
@@ -2,16 +2,23 @@ using GamePlatformManagement.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Toolbelt.Blazor.Extensions.DependencyInjection;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddHttpClient("GamePlatformManagement.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+builder.Services.AddHttpClient("GamePlatformManagement.ServerAPI", (sp, client) =>
+{
+    client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
+    client.EnableIntercept(sp);
+})   
     .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
 
 // Supply HttpClient instances that include access tokens when making requests to the server project
 builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("GamePlatformManagement.ServerAPI"));
+
+builder.Services.AddHttpClientInterceptor();
 
 builder.Services.AddApiAuthorization();
 
---
+++ GamePlatformManagement/Client/wwwroot/scripts/carousel.js
@@ -0,0 +1,8 @@
+﻿function StartCarousel(carouselId) {
+    const myCarouselElement = document.querySelector(carouselId)
+
+    const carousel = new bootstrap.Carousel(myCarouselElement, {
+        interval: 2000,
+        touch: false
+    })
+}
