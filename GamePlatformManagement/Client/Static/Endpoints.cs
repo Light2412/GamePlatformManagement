6032fb949871ba17b6be21bf8314a3b7442faf78
JERRICK BEH ZHILUN 2204985B <2204985B@student.tp.edu.sg>
2024-02-03 01:57:08 +0800

    Added Blazor List, Create, Update, View with delete function

---
+++ GamePlatformManagement/Client/Static/Endpoints.cs
@@ -0,0 +1,10 @@
+﻿namespace GamePlatformManagement.Client.Static
+{
+    public static class Endpoints
+    {
+        private static readonly string Prefix = "api";
+
+        public static readonly string GamesEndpoint = $"{Prefix}/games";
+        public static readonly string StaffsEndpoint = $"{Prefix}/staffs";
+    }
+}
